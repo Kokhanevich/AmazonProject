4a7e7fd18eb4b876c46449a487f0c67d8f487ef4
Kokhanevich <nike.k1995@gmail.com>
2020-04-22 11:31:42 +0300

    parser fixes

--- src/main/java/com/kokhanevych/amazon/service/CsvParserService.java
+++ src/main/java/com/kokhanevych/amazon/service/CsvParserService.java
@@ -4,13 +4,16 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
-import au.com.bytecode.opencsv.CSVReader;
+import javax.annotation.PostConstruct;
+
 import com.kokhanevych.amazon.entity.Review;
 import com.kokhanevych.amazon.repository.ReviewRepository;
+import com.opencsv.CSVReader;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,17 +27,19 @@ public class CsvParserService {
     @Autowired
     private ReviewRepository reviewRepository;
     private static final char DEFAULT_SEPARATOR = ',';
-    private static final String FILE_NAME = "Test.csv";
+    private static final String FILE_NAME = "Reviews.csv";
+//    private static final String FILE_NAME = "Test.csv";
     private List<String[]> strings = new ArrayList<>();
 
     private List<String[]> parseFile(String fileName) throws IOException {
         logger.trace("Init parse file method");
-        CSVReader csvReader = null;
+        CSVReader csvReader;
+        char symbol = 127;
         ClassLoader classLoader = CsvParserService.class.getClassLoader();
         File file = new File(Objects.requireNonNull(classLoader
                 .getResource(fileName)).getFile());
         try {
-            csvReader = new CSVReader(new FileReader(file), DEFAULT_SEPARATOR, '"', 1);
+            csvReader = new CSVReader(new FileReader(file, StandardCharsets.UTF_8), DEFAULT_SEPARATOR, symbol, 1);
             logger.trace("Parsing finished success");
         } catch (FileNotFoundException e) {
             logger.error(String.format("Can’t read file %s", fileName), e);
@@ -43,11 +48,12 @@ public class CsvParserService {
         return csvReader.readAll();
     }
 
-    public void addReviewToDb(String fileName) {
+    @PostConstruct
+    public void addReviewToDb() {
         try {
-            strings = parseFile(fileName);
+            strings = parseFile(FILE_NAME);
         } catch (IOException e) {
-            logger.error(String.format("Can’t parse file %s", fileName), e);
+            logger.error(String.format("Can’t parse file %s", FILE_NAME), e);
             throw new RuntimeException(e);
         }
         for (String[] stringsArr : strings) {
@@ -58,9 +64,9 @@ public class CsvParserService {
 
     private Review createReview(String[] strings) {
         Review review = new Review();
-        review.setProductId(strings[2]);
-        review.setUserId(strings[3]);
-        review.setProfileName(strings[4]);
+        review.setProductId(strings[1]);
+        review.setUserId(strings[2]);
+        review.setProfileName(strings[3]);
         review.setSummary(strings[8]);
         review.setTextReview(strings[9]);
         return review;
