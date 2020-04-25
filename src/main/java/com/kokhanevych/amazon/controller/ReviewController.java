a281162987582a134201fe730bd897f2c19a2b50
Kokhanevich <nike.k1995@gmail.com>
2020-04-25 17:55:24 +0300

    added controllers, Swagger, Travis

---
+++ src/main/java/com/kokhanevych/amazon/controller/ReviewController.java
@@ -0,0 +1,33 @@
+package com.kokhanevych.amazon.controller;
+
+import java.util.List;
+
+import com.kokhanevych.amazon.service.ReviewService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/review")
+public class ReviewController {
+
+    @Autowired
+    private ReviewService reviewService;
+
+    @GetMapping("/getProfiles/{limit}")
+    public List<String> getMostActiveUsersProfile(@PathVariable("limit") Integer limit) {
+        return reviewService.getMostActiveUserProfiles(limit);
+    }
+
+    @GetMapping("/getItems/{limit}")
+    public List<String> getMostCommentedItemsId(@PathVariable("limit") Integer limit) {
+        return reviewService.getMostCommentedItemsId(limit);
+    }
+
+    @GetMapping("/getWords/{limit}")
+    public List<String> getMostPopularWords(@PathVariable("limit") Integer limit) {
+        return reviewService.getMostUsefulWords(limit);
+    }
+}
--- src/main/java/com/kokhanevych/amazon/repository/ReviewRepository.java
+++ src/main/java/com/kokhanevych/amazon/repository/ReviewRepository.java
@@ -9,16 +9,16 @@ import org.springframework.data.jpa.repository.Query;
 public interface ReviewRepository extends JpaRepository<Review, Long> {
     @Query(value =
             "SELECT profile_name FROM reviews GROUP BY profile_name "
-                    + "ORDER BY COUNT(profile_name) DESC LIMIT ?1;",
+                    + "ORDER BY COUNT(profile_name) DESC LIMIT ?1",
             nativeQuery = true)
     List<String> getMostActiveUserProfiles(Integer limit);
 
     @Query(value =
             "SELECT product_id FROM reviews GROUP BY product_id "
-                    + "ORDER BY COUNT(product_id) DESC LIMIT ?1;",
+                    + "ORDER BY COUNT(product_id) DESC LIMIT ?1",
             nativeQuery = true)
     List<String> getMostCommentedItemsId(Integer limit);
 
-    @Query(value = "SELECT text_review from reviews;", nativeQuery = true)
+    @Query(value = "SELECT text_review from reviews", nativeQuery = true)
     List<String> getReviews();
 }
